853851027b1b382cab9a7f6ae0648ca0901da35a
QA-Anamika <anamika.gupta493@gmail.com>
2025-10-30 15:31:27 +0530

    chore: ignore env files

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,13 @@ module.exports = defineConfig({
   e2e: {
     // default support file path (where you should put Cypress.on('uncaught:exception', ...))
     supportFile: 'cypress/support/e2e.js',
-    baseUrl: "https://practice.expandtesting.com/",
-    apiBaseUrl: 'https://practice.expandtesting.com/notes/api',
+    // baseUrl: "https://practice.expandtesting.com/",
+    // apiBaseUrl: 'https://practice.expandtesting.com/notes/api',
     watchForFileChanges: false,
 
     env: {
+      TEST_BASE_URL: process.env.TEST_BASE_URL,
+      TEST_API_URL: process.env.TEST_API_URL,
       CYPRESS_API_USERNAME: process.env.CYPRESS_API_USERNAME,
       CYPRESS_API_PASSWORD: process.env.CYPRESS_API_PASSWORD,
     },
